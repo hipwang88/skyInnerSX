63d076a7d5fee51016007bbdfa88ee2feab27693
Wziiy <hipwang88@gmail.com>
2014-09-28 11:09:45 +0800

    增加窗口选择框视图类

---
+++ skyInnerSX/skyInnerSX/ISX View/skyWinBoarder.h
@@ -0,0 +1,14 @@
+//
+//  skyWinBoarder.h
+//  skyInnerSX
+//
+//  Created by skyworth on 14-9-28.
+//  Copyright (c) 2014年 wziiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// class skyWinBoarder --- 窗体选择视图
+@interface skyWinBoarder : UIView
+
+@end
