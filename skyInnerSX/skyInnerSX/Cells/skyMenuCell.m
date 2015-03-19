8be6c5487207d2ac170afb13e4aa5e1e6a91ccc1
nilin214 <skyworth@skyMacBook-Pro.local>
2015-03-19 11:25:33 +0800

    虚拟键盘国际化支持

--- skyInnerSX/skyInnerSX/Cells/skyMenuCell.m
+++ skyInnerSX/skyInnerSX/Cells/skyMenuCell.m
@@ -12,6 +12,15 @@
 
 - (void)awakeFromNib {
     // Initialization code
+    [self.menuBtn setTitle:NSLocalizedString(@"UnitControl_Menu", nil) forState:UIControlStateNormal];
+    [self.upBtn setTitle:NSLocalizedString(@"UnitControl_UP", nil) forState:UIControlStateNormal];
+    [self.downBtn setTitle:NSLocalizedString(@"UnitControl_Down", nil) forState:UIControlStateNormal];
+    [self.leftBtn setTitle:NSLocalizedString(@"UnitControl_Left", nil) forState:UIControlStateNormal];
+    [self.rightBtn setTitle:NSLocalizedString(@"UnitControl_Right", nil) forState:UIControlStateNormal];
+    [self.panelBtn setTitle:NSLocalizedString(@"UnitControl_Dis", nil) forState:UIControlStateNormal];
+    [self.signalBtn setTitle:NSLocalizedString(@"UnitControl_Signal", nil) forState:UIControlStateNormal];
+    [self.confirmBtn setTitle:NSLocalizedString(@"UnitControl_Confirm", nil) forState:UIControlStateNormal];
+    [self.quitBtn setTitle:NSLocalizedString(@"UnitControl_Exit", nil) forState:UIControlStateNormal];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
